0bb90487fc3575c6eb2b378d86fd6618d6d37848
rahulthesun82 <rahulthesun82@gmail.com>
2026-07-10 19:51:05 +0530

    feat(Attendance Functionality) : - Added Better Icons / Data Rep for Attendance History - Added Calendar Style Absent / Present Representation with Dots - Added Working Scan - Added Better Debugging for API Route for Scan not Working
    err(scan not working) - Scan is showing QR Expired repetitively

--- web/lib/helpers.ts
+++ web/lib/helpers.ts
@@ -6,3 +6,29 @@ export const toTitleCase = (str?: string | null) =>
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ')
     : ''
+
+export function formatDate(date: string): string {
+  const [year, month, day] = date.split("-").map(Number);
+
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  const getOrdinal = (n: number): string => {
+    if (n >= 11 && n <= 13) return `${n}th`;
+
+    switch (n % 10) {
+      case 1:
+        return `${n}st`;
+      case 2:
+        return `${n}nd`;
+      case 3:
+        return `${n}rd`;
+      default:
+        return `${n}th`;
+    }
+  };
+
+  return `${getOrdinal(day)} ${months[month - 1]} ${year}`;
+}
